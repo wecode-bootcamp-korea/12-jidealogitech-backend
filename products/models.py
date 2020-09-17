36b016f471ab294da351f5e93fa6685a63f8945a
jake <hongse21@gamil.com>
2020-09-17 17:40:44 +0900

    Add: product_models_firstcommit

---
+++ products/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+# from order.models import Order 
+# from account.models import Account
+
+class Main_category(models.Model):
+    name                 = models.CharField(max_length=50)
+    class Meta:
+        db_table ='main_categories'
+
+class Category(models.Model):
+    name                 = models.CharField(max_length=50)
+    main_category        = models.ForeignKey(Main_category, on_delete = models.CASCADE)
+    class Meta:
+        db_table = 'categories'
+
+class Product(models.Model):
+ 
+    product_banner       = models.CharField(max_length=1000, null=True)
+    product_teaser       = models.CharField(max_length=1000, null=True)
+    product_key_feature  = models.CharField(max_length=1000, null=True)
+    product_title        = models.CharField(max_length=50)
+    product_details      = models.CharField(max_length=50)
+    product_price        = models.DecimalField(max_digits=5, decimal_places=2)
+    product_note         = models.CharField(max_length=50)
+    additional_feature   = models.CharField(max_length=1000, null=True)
+    color_model          = models.CharField(max_length=50, null=True)
+    specs_details        = models.CharField(max_length=1000)
+    recommended_products = models.CharField(max_length=30, null=True )
+    category             = models.ForeignKey(Category, on_delete = models.CASCADE)
+    thumnail_url         = models.URLField(max_length  = 100)
+    color_model          = models.CharField(max_length=100, null=True)
+
+    class Meta:
+        db_table = 'products'
+
+class Thumnail(models.Model):
+    thumnail_url         = models.ForeignKey(Product, on_delete = models.CASCADE)
+    class Meta:
+        db_table = 'thumnails'
+
+class Order_Product(models.Model):
+    # order               = models.ForeignKey(Order, on_delete = models.CASCADE)
+    product             = models.ForeignKey(Product, on_delete = models.CASCADE)
+    count               = models.IntegerField(default=0)
+    class Meta:
+        db_table = 'order_products'
