ffae17a27c7597fc3da54edf454e77eae46cc8b5
gorusair <67059323+gorusair@users.noreply.github.com>
2020-09-19 12:40:58 +0900

    Revert "[구동현] Add: account.models.py  수정 "

--- account/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-# from account.views import CreateView, LoginView
-
-urlpatterns = [
-#     path('/create', CreateView.as_view()),
-#     path('/login',  LoginView.as_view())
-]
