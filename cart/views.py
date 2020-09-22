5c8ab4fd1d9c2bf3edc84af61117c993094bc8d4
구동현 <gorusair@gmail.com>
2020-09-22 15:15:00 +0900

    Add:cart.models.py

---
+++ cart/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# from django.views import View
+# from django.http  import JsonResponse, HttpResponse
+
+# from cart.models import Cart
+
+
+# class CartView(View):
+#     def post(self, request):
+#         try:
+#             data        = jsno
