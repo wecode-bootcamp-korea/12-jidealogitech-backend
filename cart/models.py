0172ea6c307e12a7faf1520c328fa123589e33fd
구동현 <gorusair@gmail.com>
2020-09-22 17:58:39 +0900

    Add:cart.models.py.second

--- cart/models.py
+++ cart/models.py
@@ -3,13 +3,10 @@ from account.models import Account
 # from product.models import Product
 
 class Cart(models.Model):
-    cart_id            = models.ForeignKey(Account, on_delete = models.CASCADE)
-    # product_title      = models.ForeignKEY(Product, on_delete = models.CASCAED)
-    image              = models.URLField(max_length=200)
+    account            = models.ForeignKey(Account, on_delete = models.CASCADE)
+    # product           = models.ForeignKEY(Product, on_delete = models.CASCAED)
     count              = models.IntegerField(default=0)
-    price              = models.IntegerField(default=0)
-    delete             = models.CharField(max_length=50)
     
-   
+    
     class Meta:
         db_table : 'carts'
